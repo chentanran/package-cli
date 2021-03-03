88167871c5662f377c91c0cf54c54d215ce3cdad
chentanran <215920697@qq.com>
2021-03-03 22:59:36 +0800

    lerna

--- package-cli-lib/lib/index.js
+++ package-cli-lib/lib/index.js
@@ -1,5 +1,8 @@
 module.exports = {
     sum(a,b) {
         return a + b
+    },
+    init() {
+        console.log('初始化init')
     }
 }
--- package-cli/bin/index.js
+++ package-cli/bin/index.js
@@ -1,6 +1,24 @@
 #!/usr/bin/env node
 
 const lib = require('package-cli-lib')
-console.log(lib)
 
-console.log('哇哈哈真好喝aaaaasqqqss')
+const common = require('process').argv
+
+if (common[2].startsWith('--') || common[2].startsWith('-')) {
+    const option = common[2].replace(/--|-/g, '')
+    if (option === 'version' || option === 'v') {
+        console.log('1.0.0')
+    }
+}
+
+// console.log(common)
+
+// if (common[2]) {
+//     if (lib[common[2]]) {
+//         lib[common[2]]()
+//     } else {
+//         console.log('没有此命令')
+//     }
+// } else {
+//     console.log('请输入命令')
+// }

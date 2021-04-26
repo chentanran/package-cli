c98c21980fe785695a88181bb50ae5a1a8c5af61
chentanran <215920697@qq.com>
2021-04-26 23:50:33 +0800

    .

--- package-cli/bin/index.js
+++ package-cli/bin/index.js
@@ -1,24 +1,39 @@
 #!/usr/bin/env node
 
-const lib = require('package-cli-lib')
+const commander = require('commander')
+const pkg = require('../package.json')
 
-const common = require('process').argv
+// 获取 commander 的单例
+// const { program } = commander
 
-if (common[2].startsWith('--') || common[2].startsWith('-')) {
-    const option = common[2].replace(/--|-/g, '')
-    if (option === 'version' || option === 'v') {
-        console.log('1.0.0')
-    }
-}
+// 手动实例化一个 Commander
+const program = new commander.Command()
 
-// console.log(common)
+program
+    .name(Object.keys(pkg.bin)[0])
+    .usage('<command> [options]')
+    .version(pkg.version)
+    .option('-d, --debug', '是否开启调试模式', false)
+    .option('-e, --envName <envName>', '获取环境变量名称')
 
-// if (common[2]) {
-//     if (lib[common[2]]) {
-//         lib[common[2]]()
-//     } else {
-//         console.log('没有此命令')
-//     }
-// } else {
-//     console.log('请输入命令')
-// }
+// command 注册命令
+const clone = program.command('clone <source> [destination]')
+clone
+    .description('clone a repository into a newly created directory')
+    .option('-f, --fetch', '是否强制克隆')
+    .action((source, destination) => {
+        console.log('clone command called');
+    })
+
+// addCommand 注册子命令
+const service = new commander.Command('service')
+service
+    .command('start [port]')
+    .description('start service at some port')
+    .action((port) => {
+        console.log('do service start', port)
+    })
+
+program.addCommand(service)
+
+program.parse(process.argv)

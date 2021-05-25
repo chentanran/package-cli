9ecd3b613fce523a58c2ef8ee32f9610d127d606
chentanran <215920697@qq.com>
2021-05-25 22:30:38 +0800

    .

--- package-cli-template/package-cli-template-custom-vue/index.js
+++ package-cli-template/package-cli-template-custom-vue/index.js
@@ -1,9 +1,12 @@
+const path = require('path')
 const fse = require('fs-extra')
 const ejs = require('ejs')
+const inquirer = require('inquirer')
+const log = require('@package-cli-dev/log')
 
 async function ejsRender(option) {
-  const dir = process.cwd()
-  const projectInfo = option.projectInfo
+  const dir = option.targetPath
+  const projectInfo = option.data
   return new Promise((resolve, reject) => {
       require('glob')('**', {
           cwd: dir,
@@ -18,12 +21,14 @@ async function ejsRender(option) {
               return new Promise((res, rej) => {
                   ejs.renderFile(filePath, projectInfo, {}, (err, result) => {
                       if (err) {
-                          rej(err)
+                        rej(err)
                       } else {
-                          fse.writeFileSync(filePath, result)
-                          res(result)
+                        fse.writeFileSync(filePath, result)
+                        res(result)
                       }
                   })
+              }).catch(() => {
+                reject()
               })
           })).then(() => {
               resolve()
@@ -35,6 +40,7 @@ async function ejsRender(option) {
 }
 
 async function install(options) {
+  const projectPrompt = []
   const descriptionPrompt = {
     type: 'input',
     name: 'description',
@@ -46,7 +52,7 @@ async function install(options) {
   }
   projectPrompt.push(descriptionPrompt)
   const projectInfo = await inquirer.prompt(projectPrompt)
-  options.description = projectInfo.description
+  options.projectInfo.description = projectInfo.description
   try {
     // 获取缓存文件路径
     // 获取当前目录路径
@@ -56,20 +62,16 @@ async function install(options) {
     fse.ensureDirSync(targetPath)
     // 复制文件或目录
     fse.copySync(sourcePath, targetPath)
+
+    // 从接口中读取忽略文件
+    let templateIgnore = options.templateInfo.ignore || []
+    const ignore = ['node_modules/**', ...templateIgnore]
+    await ejsRender({ ignore, data: options.projectInfo, targetPath })
   } catch(e) {
       throw e
   } finally {
-      spinner.stop(true)
       log.success('模板安装成功')
   }
-  // 从接口中读取忽略文件
-  let templateIgnore = options.templateInfo.ignore || []
-  const ignore = ['node_modules/**', ...templateIgnore]
-  await ejsRender({ ignore, options })
-  // 依赖安装
-  // const { installCommand, startCommand } = this.templateInfo
-  // await this.execCommand(installCommand, '依赖安装过程失败！')
-  // await this.execCommand(startCommand, '启动过程失败！')
 }
 
 module.exports = install

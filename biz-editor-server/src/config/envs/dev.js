f80f617cc9b102558a73bdabae7c606c40d2c08f
chentanran <215920697@qq.com>
2022-03-01 14:43:13 +0800

    redis

--- biz-editor-server/src/config/envs/dev.js
+++ biz-editor-server/src/config/envs/dev.js
@@ -1,4 +1,5 @@
 module.exports = {
+	// mysql 连接配置
 	mysqlConf: {
 		host: '121.36.199.250',
 		user: 'root',
@@ -12,5 +13,12 @@ module.exports = {
 		host: 'localhost',
 		port: '27017',
 		dbName: 'lego_course',
+	},
+
+	// redis 连接配置
+	redisConf: {
+		port: '6379',
+		host: '121.36.199.250',
+		password: 'abc123'
 	},
 }
